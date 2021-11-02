0f1fabac1724a0fc7c346d6c9526b4ff91e743d5
Debyanshu Tiwari <88588383+debyanshutiwari@users.noreply.github.com>
2021-11-02 09:15:17 +0530

    Add files via upload

---
+++ Double_a_number_using_operator.java
@@ -0,0 +1,15 @@
+// output =114
+
+
+package debyanshu;
+
+public class Tiwari {
+    public static void main(String[] args) {
+        int a=57;
+        System.out.println(tiw(a));
+    }
+ private    static int tiw(int a){
+        return a<<1; //  it will give double of number
+
+    }
+}
---
+++ Half_of_number_using_operator.java
@@ -0,0 +1,15 @@
+//  output :- 59.0
+
+
+package debyanshu;
+
+public class Tiwari {
+    public static void main(String[] args) {
+       int  a=59;
+        System.out.println(tiw(a));
+    }
+ private    static float tiw(int a){
+        return a>>1; // // formula a/2^b here b is 1
+
+    }
+}
